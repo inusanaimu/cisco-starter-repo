66c3b8ddc691311a2c032d0051723d4c802fa8f4
Inusa Naimu <inusanaimu@gmail.com>
2022-11-28 20:53:40 +0100

    TailwindCss added and other componets

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
-
+import Header from './components/Header';
+import Card from './ui/Card';
 function App() {
-  return ;
+  return (
+    <div>
+
+      <Card>
+      <Header/>
+
+      </Card>
+
+    </div>
+
+  );
 }
 
 export default App;
---
+++ sextant/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='text-align:center; margin:auto' >Sextant</h1>
+    </div>
+  )
+}
+
+export default Header
